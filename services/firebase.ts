c610db275fed500c6b9951fda8a5aa805fc6d76a
Rafael Molina <rafael@molina-aquino.com>
2025-04-22 09:42:11 -0400

    Refactor Firebase configuration to remove console log and update Firestore initialization with a specific project name.

--- services/firebase.ts
+++ services/firebase.ts
@@ -4,14 +4,6 @@ import {getFirestore} from "firebase/firestore";
 
 // const googleServices = require("../google-services.json");
 // Your Firebase configuration
-console.log("FIREBASE CONFIG", {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-});
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -25,6 +17,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore
-const firestore = getFirestore(app);
+const firestore = getFirestore("ratesve");
 
 export {firestore};
